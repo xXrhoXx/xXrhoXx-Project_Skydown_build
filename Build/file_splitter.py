f6a2b30a36535e15ee6032bff5dcb6a26c210205
Rho- дес <79406648+xXrhoXx@users.noreply.github.com>
2026-06-22 02:17:33 +0800

    chunking data

---
+++ Build/file_splitter.py
@@ -0,0 +1,11 @@
+chunk_size = 50 * 1024 * 1024
+
+with open("Sky-Down_web.data", "rb") as f:
+    i = 0
+    while True:
+        chunk = f.read(chunk_size)
+        if not chunk:
+            break
+        with open(f"game.part.{i:03}", "wb") as out:
+            out.write(chunk)
+        i += 1
